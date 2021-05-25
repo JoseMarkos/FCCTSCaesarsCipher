072309a9181606bc80f14b92249924ed76472d43
Marcos <jkmarcosb@gmail.com>
2021-05-25 00:38:23 -0600

    adding gendle reading and kind of clean code

--- rot13.ts
+++ rot13.ts
@@ -1,36 +1,29 @@
-export default function (str: string) {
-  let alphaNumerics: number[] = [];
-  let collection: string[][]  = [];
-  let words: string[]         = [];
+export default function (sentence: string) {
+  let rot13CharCodes: string[][]  = [];
+  let words: string[]             = [];
 
-  str.split(' ').forEach(x => {
-    let arr: number[] = x.split('').map(x => {
-      const isNonAlphaNumeric = x.match(/[^A-Z]/);
-      
-      if (isNonAlphaNumeric)
-        alphaNumerics.push(x.charCodeAt(0));
-
-      return  x.charCodeAt(0)
-    });
-
-    collection.push(arr.map(x => {
-      const sum: number = x + 13;
-      const max         = 'Z'.charCodeAt(0);
-      
-      if (alphaNumerics.includes(x))
-        return String.fromCharCode(x);
-
-      if (sum > max) {
-        let diff    = sum - max;
-        let newCode = 'A'.charCodeAt(0) + diff - 1;
-        return String.fromCharCode(newCode);
-      }
-
-      return String.fromCharCode(sum);
-    }));
+  sentence.split(' ').forEach(word => {
+    let charCodes: number[] = word.split('').map(char => char.charCodeAt(0));
+    rot13CharCodes.push(charCodes.map(x => getRot13Code(x)));
   });
-  
-  collection.forEach(x => words.push(x.join('')))
-  
+
+  words = rot13CharCodes.map(x => x.join(''));
   return words.join(' ');
+}
+
+const getRot13Code = (x: number) => {
+  const sum: number       = x + 13;
+  const max               = 'Z'.charCodeAt(0);
+  const min               = 'A'.charCodeAt(0);
+
+  if (x < min)
+    return String.fromCharCode(x);
+
+  if (sum > max) {
+    let diff    = sum - max;
+    let newCode = min + diff - 1;
+    return String.fromCharCode(newCode);
+  }
+
+  return String.fromCharCode(sum);
 }
