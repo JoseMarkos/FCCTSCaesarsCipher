d376a9c0cf20a7d5a4314f61662db7f3aa0ff7ab
Marcos <jkmarcosb@gmail.com>
2021-05-26 22:50:40 -0600

    improving const and functions naming

--- rot13.ts
+++ rot13.ts
@@ -1,27 +1,27 @@
 export default function (sentence: string) {
-  let rot13CharCodes: string[][]  = [];
-  let words: string[]             = [];
-
+  let wordsRot13Splited: string[][]  = [];
+  
   sentence.split(' ').forEach(word => {
-    let charCodes: number[] = word.split('').map(char => char.charCodeAt(0));
-    rot13CharCodes.push(charCodes.map(x => getRot13Code(x)));
+    const charCodes: number[] = word.split('').map(char => char.charCodeAt(0));
+    wordsRot13Splited.push(charCodes.map(x => getRot13CharsFromCode(x)));
   });
+  
+  const words: string[] = wordsRot13Splited.map(x => x.join(''));
 
-  words = rot13CharCodes.map(x => x.join(''));
   return words.join(' ');
 }
 
-const getRot13Code = (x: number) => {
-  const sum: number       = x + 13;
-  const max               = 'Z'.charCodeAt(0);
-  const min               = 'A'.charCodeAt(0);
-
+const getRot13CharsFromCode = (x: number) => {
+  const sum: number = x + 13;
+  const max         = 'Z'.charCodeAt(0);
+  const min         = 'A'.charCodeAt(0);
+  
   if (x < min)
-    return String.fromCharCode(x);
-
+  return String.fromCharCode(x);
+  
   if (sum > max) {
-    let diff    = sum - max;
-    let newCode = min + diff - 1;
+    const diff    = sum - max;
+    const newCode = min + diff - 1;
     return String.fromCharCode(newCode);
   }
 
